d1a997017147172d82c0f30832d05f804cc39f3e
Michaël P.O <mike.pambo@gmail.com>
2013-11-28 22:56:18 +0000

    init

---
+++ src/labs/mikangali/com/Main.java
@@ -0,0 +1,22 @@
+package com.test.greetingandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class Main extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void onClick(View v){
+        Intent intent = new Intent(getBaseContext(), SignoutActivity.class);
+        intent.putExtra("EXTRA_CARTON_ID", v.getId());
+        startActivity(intent);
+    }
+}
